6cd8deae286c1a14e78a635e1f350de256e57af3
hamidrmz <hamidramazani1990@gmail.com>
2026-06-20 16:23:24 +0330

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{background:"#121212",color:"#fff",fontFamily:"sans-serif"}}>
+      <section style={{padding:"80px 20px",textAlign:"center"}}>
+        <h1 style={{color:"#C9A46A",fontSize:"50px"}}>ONE WOOD</h1>
+        <p>طراحی کابینت و دکوراسیون داخلی مدرن</p>
+        <p>Urmia | 09147233274</p>
+      </section>
+    </div>
+  )
+}
